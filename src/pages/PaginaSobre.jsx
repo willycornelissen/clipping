7d90b05327b444a678ee63f706a0db70d7bd7e3f
Willy Garabini Cornelissen <willy.cornelissen@gmail.com>
2026-08-24 08:45:11 -0300

    Corrections: space onde+Castália, bold links with correct URLs and target

--- src/pages/PaginaSobre.jsx
+++ src/pages/PaginaSobre.jsx
@@ -5,8 +5,7 @@ export function PaginaSobre() {
     <article className="noticia">
       <h2 className="fonte">Sobre</h2>
       <p>
-A Cidade da Residência, habitualmente tratada como a Capital do país onde
-<strong><a href="https://diario-de-castalia.ghost.io/about/" target="_blank" rel="noopener noreferrer">Castália</a></strong> está inserida, funciona como a antítese geográfica, política e
+A Cidade da Residência, habitualmente tratada como a Capital do país onde Castália está inserida, funciona como a antítese geográfica, política e
 filosófica da província pedagógica de Castália.
       </p>
       <p>
@@ -15,14 +14,14 @@ filosófica da província pedagógica de Castália.
       </p>
       <dl>
         <div>
-          <dt><a href="www.castalia.net.br" target="_blank" rel="noopener noreferrer">Diário de Castália</a></dt>
+          <dt><strong><a href="https://www.castalia.net.br" target="_blank" rel="noopener noreferrer">Diário de Castália</a></strong></dt>
           <dd>
             Representa a Vita Contemplativa — o espírito, a atemporalidade, a ordem
             abstrata e a torre de marfim.
           </dd>
         </div>
         <div>
-          <dt><a href="clipping.willy.dev.br" target="_blank" rel="noopener noreferrer">Diário da Capital</a></dt>
+          <dt><strong><a href="https://clipping.willy.dev.br">Diário da Capital</a></strong></dt>
           <dd>
             Representa a Vita Activa — a matéria, a história, o conflito moral, a
             família e a responsabilidade cívica no mundo real.
